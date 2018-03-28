f1ede0c5931049d587624544f001aa45cf17690f
Andre <Andre@Andre-PC>
2018-03-28 12:51:07 +0200

    4 Commit

---
+++ SarchKISS/src/edu/hm/cs/rs/arch18/a01_kiss/Csv.java
@@ -0,0 +1,14 @@
+package edu.hm.cs.rs.arch18.a01_kiss;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class Csv implements CSVReader {
+
+	@Override
+	public String[][] read(Reader reader) throws IOException, IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
